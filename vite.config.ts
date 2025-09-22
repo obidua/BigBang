1b0a5f583472a24b7716d4291530263c775f7539
obidua <64002560+obidua@users.noreply.github.com>
2025-09-22 11:23:14 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
