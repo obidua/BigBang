c47a304e2246ae2c70cb8c066387631e80a36791
obidua <64002560+obidua@users.noreply.github.com>
2025-09-22 11:23:00 +0530

    Fix Cross-Origin-Opener-Policy Error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
